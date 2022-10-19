fba6013416f4867fc51a1c26bde18e26bafb1870
asol25 <usool.203@gmail.com>
2022-10-19 17:39:26 +0700

    deploy production vercel

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://nextjs.org/docs" className={styles.card}>``
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
